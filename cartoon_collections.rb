338f4d889f572c2d94fa2d00b06d6ef725ece003
Andrey Syroyezhkin <asyroyez@gmail.com>
2017-10-23 16:23:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,8 @@ def summon_captain_planet(planeteer_calls)
 end
 
 def long_planeteer_calls(words)
-  words.any? { |word| word.length > 4 } || words.all? { |word| word.length < 4 }
+  words.any? { |word| word.length > 4 } or
+  words.all? { |word| word.length < 4 }
 end
 
 def find_the_cheese(list_of_food)
